c7d0b5201c0dea89fdea5d5c600507462874d787
Jacob <jt7991@unm.edu>
2017-11-15 21:03:53 -0700

    Added GUIX and GUIY

--- src/Station.java
+++ src/Station.java
@@ -8,6 +8,7 @@ public class Station extends Component
   private Component track, leftComponent;
   private String stationName;
   private Train train;
+  private int guiX, guiY;
 
   public Station(int trackX, int trackY, String stationName)
   {
@@ -18,19 +19,24 @@ public class Station extends Component
   }
   public void display(GraphicsContext gc)
   {
+    guiX = Reference.length*trackX + 50;
+    guiY = Reference.y*trackY+200 - Reference.length;
     gc.setFill(Reference.COLORS[stationName.charAt(0)% Reference.COLORS.length]);
-    gc.fillRect(Reference.length*trackX + 50, Reference.y*trackY+200 - Reference.length, Reference.length ,Reference.length);
+    gc.fillRect(guiX, guiY, Reference.length ,Reference.length);
 
   }
 
   public void setLeftComponent(Component component)
   {
+
     leftComponent = component;
   }
 
   @Override
   public synchronized void acceptMessage(String message, ArrayList<Component> path, boolean sending)
   {
+    if(rightComponent.getClass().toString().contains("Station")) track = leftComponent;
+    else track = rightComponent;
     if(sending)
     {
       track.notify();
